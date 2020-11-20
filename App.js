3a728ca8d012816e5a838a45c0048176f62a0139
ermyas7 <ermyas.fekadu@gmail.com>
2020-11-20 07:04:07 +0300

    innitial commit

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  View,Text
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <View>
+      <Text>Main App</Text>
+    </View>
+  );
+};
+
+
+export default App;
